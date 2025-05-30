6f5a074dda4be953dd6472c29e5934b7656aa567
Fehleh <fehleh2021@gmail.com>
2025-05-30 17:44:14 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
 
-import Head from 'next/head'
-import Header from '../components/Header'
-import '../styles/global.css'
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>ProfPronto</title>
-        <meta name="description" content="Atividades adaptadas e prontas para imprimir." />
-      </Head>
-      <Header />
-      <main style={{ padding: '2rem' }}>
-        <h1>Bem-vindo ao ProfPronto!</h1>
-        <p>Aqui você encontra atividades adaptadas e lúdicas para alunos com dificuldade de aprendizagem, prontas para serem aplicadas em sala de aula.</p>
-      </main>
-    </>
-  )
+    <main>
+      <img src="/ProfPronto_logo_corrigida.png" alt="ProfPronto Logo" style={{ maxWidth: '300px', marginBottom: '2rem' }} />
+      <h1>Bem-vindo ao ProfPronto!</h1>
+      <p>Atividades prontas, inclusivas e divertidas para sua sala de aula.</p>
+    </main>
+  );
 }
